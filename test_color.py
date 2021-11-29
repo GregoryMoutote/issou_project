4c75cdb05e1056e916f3af0c136b70c7cbe451fd
Benjamin-Eldo <84413803+Benjamin-Eldo@users.noreply.github.com>
2021-11-29 20:21:21 +0100

    ajout detection de couleur

---
+++ test_color.py
@@ -0,0 +1,66 @@
+
+# Python code for Multiple Color Detection
+  
+  
+import numpy as np
+import cv2
+  
+  
+# Capturing video through webcam
+image = cv2.imread("tableau.jpg")
+image = cv2.resize(image, (1500,675))
+
+arrayPoint = []
+# Start a while loop
+
+# Convert the imageFrame in 
+# BGR(RGB color space) to 
+# HSV(hue-saturation-value)
+# color space
+hsvFrame = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+# Set range for red color and 
+# define mask
+red_lower = np.array([136, 87, 111], np.uint8)
+red_upper = np.array([180, 255, 255], np.uint8)
+red_mask = cv2.inRange(hsvFrame, red_lower, red_upper)
+
+    
+# Morphological Transform, Dilation
+# for each color and bitwise_and operator
+# between imageFrame and mask determines
+# to detect only that particular color
+kernal = np.ones((5, 5), "uint8")
+    
+# For red color
+red_mask = cv2.dilate(red_mask, kernal)
+res_red = cv2.bitwise_and(image, image, 
+                            mask = red_mask)
+
+
+# Creating contour to track red color
+contours, hierarchy = cv2.findContours(red_mask,
+                                        cv2.RETR_TREE,
+                                        cv2.CHAIN_APPROX_SIMPLE)
+
+
+for pic, contour in enumerate(contours):
+    area = cv2.contourArea(contour)
+    if(area > 300):
+        x, y, w, h = cv2.boundingRect(contour)
+        arrayPoint.append((x,y))
+        image = cv2.rectangle(image, (x, y), 
+                                    (x + w, y + h), 
+                                    (0, 0, 255), 2)
+            
+        cv2.putText(image, "Red Colour", (x, y),
+                    cv2.FONT_HERSHEY_SIMPLEX, 1.0,
+                    (0, 0, 255))    
+
+
+print(arrayPoint[0])
+pts1 = np.float32((arrayPoint[0],arrayPoint[1],arrayPoint[2]))
+print(type(arrayPoint))
+# Program Termination
+cv2.imshow("Multiple Color Detection in Real-TIme", image)
+cv2.waitKey()
