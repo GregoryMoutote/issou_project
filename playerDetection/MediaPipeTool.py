197e888a80e732fe596c940f13f9540c44320ad5
Benjamin-Eldo <84413803+Benjamin-Eldo@users.noreply.github.com>
2021-12-11 11:53:16 +0100

    Left and right hand distinction (only back-end)

--- playerDetection/MediaPipeTool.py
+++ playerDetection/MediaPipeTool.py
@@ -4,6 +4,8 @@ import cv2
 class MediaPipeTool :
     def __init__(self):
         self.cap = cv2.VideoCapture(0)
+        self.leftHand = ()
+        self.rightHand = ()
 
     def initHandCapture(self):
         self.mp_hands = mp.solutions.hands
@@ -22,10 +24,16 @@ class MediaPipeTool :
             image_height, image_width, _ = image.shape
 
             result = []
+            self.leftHand = ()
+            self.rightHand = ()
             if resultsHand.multi_hand_landmarks:
                 for num, hand in enumerate(resultsHand.multi_hand_landmarks):
                     hand_x = hand.landmark[self.mp_hands.HandLandmark.MIDDLE_FINGER_MCP].x*image_width*1920/800
                     hand_y = hand.landmark[self.mp_hands.HandLandmark.MIDDLE_FINGER_MCP].y*image_height*1080/600
+                    if resultsHand.multi_handedness[num].classification[0].label == "Right":
+                        self.rightHand = (hand_x,hand_y)
+                    if resultsHand.multi_handedness[num].classification[0].label == "Left":
+                        self.leftHand = (hand_x,hand_y)
                     result.append((hand_x,hand_y))
 
             return result
