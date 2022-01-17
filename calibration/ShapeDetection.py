6214d9b9b760e744bc28e0b2508cebf07d28ade4
Benjamin-Eldo <84413803+Benjamin-Eldo@users.noreply.github.com>
2022-01-17 10:42:41 +0100

    ShapeDetection comments and contour preview

--- calibration/ShapeDetection.py
+++ calibration/ShapeDetection.py
@@ -2,16 +2,11 @@ import cv2
 
 class ShapeDetection():
 
-    def __init__(self, displayPreview:bool):
-        self.corners = []
-        self.isDisplaying = displayPreview
 
     def __init__(self):
         self.corners = []
         self.isDisplaying = False
-
-
-
+        self.webcam = None
 
     def initCamera(self):
         self.webcam = cv2.VideoCapture(0)
@@ -19,54 +14,62 @@ class ShapeDetection():
 
 
     def closeCamera(self):
-        self.webcam.release()
-        print("CAMERA CLOSED...")
+        if self.webcam is not None:
+            self.webcam.release()
+            print("CAMERA CLOSED...")
 
     def detectBoard(self):
-        _, img = self.webcam.read()
+        if self.webcam is not None:
 
-        gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+            _, img = self.webcam.read()
 
-        _, threshold = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
+            gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-        contours, _ = cv2.findContours(
-            threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+            _, threshold = cv2.threshold(gray, 127, 255, cv2.THRESH_BINARY)
 
-        contourSize = []
-        for contour in contours:
-            contourSize.append(cv2.contourArea(contour))
+            contours, _ = cv2.findContours(threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-        index = contourSize.index(max(contourSize))
+            contourSize = []
 
-        approx = cv2.approxPolyDP(contours[index], 0.01 * cv2.arcLength(contours[index], True), True)
+            #Detection de la plus grosse forme
+            for contour in contours:
+                contourSize.append(cv2.contourArea(contour))
+            index = contourSize.index(max(contourSize))
 
-        cv2.drawContours(img, [contours[index]], 0, (0, 0, 255), 5)
 
-        contour_corner = []
+            approx = cv2.approxPolyDP(contours[index], 0.01 * cv2.arcLength(contours[index], True), True)
 
-        corner = approx.ravel()
-        i = 0
+            cv2.drawContours(img, [contours[index]], 0, (0, 0, 255), 5)
 
-        for j in corner:
-            if (i % 2 == 0):
-                x = corner[i]
-                y = corner[i + 1]
+            contour_corner = []
 
-                # String containing the co-ordinates.
-                string = str(x) + " " + str(y)
-                contour_corner.append(string)
+            corner = approx.ravel()
 
-                if (i != 0):
-                    # text on remaining co-ordinates.
-                    cv2.putText(img, string, (x, y),
-                                cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
-            i = i + 1
-        self.corners.append(contour_corner)
-        print("FOUND " + str(len(self.corners))+" SHAPE(S) WITH "+str(len(self.corners[0]))+" DIMENSIONS...")
+            i = 0
 
-        if (self.isDisplaying):
-            while 1:
-                cv2.imshow('Flux Camera', img)
+            ##Detection des coordonnees du contour
+            for j in corner:
+                if (i % 2 == 0):
+                    x = corner[i]
+                    y = corner[i + 1]
 
-                if cv2.waitKey(10) & 0xFF == ord('q'):
-                    break
+                    contour_corner.append((x,y))
+
+                    if (self.isDisplaying):
+                        string = str(x) + " " + str(y)
+
+                        if (i != 0):
+                            cv2.putText(img, string, (x, y),
+                                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
+                i = i + 1
+            self.corners.append(contour_corner)
+
+
+            if (self.isDisplaying):
+                while 1:
+                    cv2.imshow('Flux Camera', img)
+
+                    if cv2.waitKey(10) & 0xFF == ord('q'):
+                        break
+        else:
+            print("FLUX CAMERA NON INITIALISE, AUCUN POINT RETOURNE...")
--- test_main.py
+++ test_main.py
@@ -4,6 +4,8 @@ from MediaPipeTool import *
 from calibration import ShapeDetection
 
 test_util = ShapeDetection.ShapeDetection()
+test_util.isDisplaying = True
 test_util.initCamera()
 test_util.detectBoard()
+print("FOUND " + str(len(test_util.corners))+" SHAPE(S) WITH "+str(len(test_util.corners[0]))+" DIMENSIONS...")
 test_util.closeCamera()
