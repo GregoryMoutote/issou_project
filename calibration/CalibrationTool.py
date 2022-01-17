e64991b9afc9030b44a434144d92c813ac800396
Benjamin-Eldo <84413803+Benjamin-Eldo@users.noreply.github.com>
2022-01-17 16:26:53 +0100

    CalibrationTool : Methode triPoint (not working correctly)

--- calibration/CalibrationTool.py
+++ calibration/CalibrationTool.py
@@ -24,24 +24,68 @@ class CalibrationTool:
 
     def getPoints(self):
         self.shape_util = ShapeDetection.ShapeDetection()
-        self.shape_util.webcam = self.webcam
+        self.shape_util.initCamera()
         print("RECUPERATION DES POINTS...")
         while len(self.matrix) != 4:
             self.matrix = self.shape_util.detectBoard()
         print(self.matrix)
+        self.shape_util.closeCamera()
 
     def calcMatrix(self):
-        img = self.webcam.read()
+        self.initCamera()
+        _, img = self.webcam.read()
 
         rows, cols, ch = img.shape
 
+
+
         self.pts1 = np.float32([[self.matrix[0][0], self.matrix[0][1]], [self.matrix[3][0], self.matrix[3][1]],
                                 [self.matrix[1][0], self.matrix[1][1]], [self.matrix[2][0], self.matrix[2][1]]])
+
+
         self.pts2 = np.float32([[0, 0], [cols, 0], [0, rows], [cols, rows]])
 
         self.M = cv2.getPerspectiveTransform(self.pts1, self.pts2)
 
         self.isDone = True
+        self.closeCamera()
+
+    def triPoint(self):
+        firstSum = self.matrix[0][0] + self.matrix[0][1]
+        secondSum = self.matrix[1][0] + self.matrix[1][1]
+        thirdSum = self.matrix[2][0] + self.matrix[2][1]
+        fourthSum = self.matrix[3][0] + self.matrix[3][1]
+
+        points = [self.matrix[0], self.matrix[1],
+                  self.matrix[2], self.matrix[3]]
+        print(points)
+        print(self.pts1)
+
+        if firstSum < secondSum & firstSum < thirdSum & firstSum < fourthSum:
+            self.pts1[0] = points[0]
+            del points[0]
+        elif secondSum < firstSum & secondSum < thirdSum & secondSum < fourthSum:
+            self.pts1[0] = points[1]
+            del points[1]
+        elif thirdSum < firstSum & thirdSum < secondSum & thirdSum < fourthSum:
+            self.pts1[0] = points[2]
+            del points[2]
+        else:
+            self.pts1[0] = points[3]
+            del points[3]
+
+        if firstSum > secondSum & firstSum > thirdSum & firstSum > fourthSum:
+            self.pts1[3] = points[0]
+            del points[0]
+        elif secondSum > firstSum & secondSum > thirdSum & secondSum > fourthSum:
+            self.pts1[3] = points[1]
+            del points[1]
+        elif thirdSum > firstSum & thirdSum > secondSum & thirdSum > fourthSum:
+            self.pts1[3] = points[2]
+            del points[2]
+        else:
+            self.pts1[3] = points[3]
+            del points[3]
 
     def calibratePicture(self, img, preview: bool):
         rows, cols, ch = img.shape
