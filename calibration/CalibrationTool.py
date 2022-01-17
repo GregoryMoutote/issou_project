a70c5b8369959bb2ad8f485620ce168d22331a10
Benjamin-Eldo <84413803+Benjamin-Eldo@users.noreply.github.com>
2022-01-17 15:11:55 +0100

    Some fixes

--- calibration/CalibrationTool.py
+++ calibration/CalibrationTool.py
@@ -3,7 +3,8 @@ import matplotlib.pyplot as plt
 import numpy as np
 from calibration import ShapeDetection
 
-class CalibrationTool :
+
+class CalibrationTool:
     def __init__(self):
         self.matrix = []
         self.webcam = None
@@ -14,7 +15,6 @@ class CalibrationTool :
     def initCamera(self):
         self.webcam = cv2.VideoCapture(0)
         print("CAMERA INITIALISATION...")
-
 
     def closeCamera(self):
         if self.webcam is not None:
@@ -27,37 +27,30 @@ class CalibrationTool :
         print("RECUPERATION DES POINTS...")
         while len(self.matrix) != 4:
             self.matrix = self.shape_util.detectFromPicture()
-        print (self.matrix)
+        print(self.matrix)
 
     def calcMatrix(self):
         img = cv2.imread("calibration/table.jpg")
 
         rows, cols, ch = img.shape
 
-
-        self.pts1 = np.float32([[self.matrix[0][0], self.matrix[0][1]],[self.matrix[3][0], self.matrix[3][1]],
-                           [self.matrix[1][0], self.matrix[1][1]],[self.matrix[2][0], self.matrix[2][1]]])
+        self.pts1 = np.float32([[self.matrix[0][0], self.matrix[0][1]], [self.matrix[3][0], self.matrix[3][1]],
+                                [self.matrix[1][0], self.matrix[1][1]], [self.matrix[2][0], self.matrix[2][1]]])
         self.pts2 = np.float32([[0, 0], [cols, 0], [0, rows], [cols, rows]])
 
         self.M = cv2.getPerspectiveTransform(self.pts1, self.pts2)
 
-
-    def calibratePicture (self, img, preview:bool):
+    def calibratePicture(self, img, preview: bool):
         rows, cols, ch = img.shape
 
-        dst = cv2.warpPerspective(img, self.M, (cols,rows))
+        dst = cv2.warpPerspective(img, self.M, (cols, rows))
         if (preview):
             plt.subplot(121), plt.imshow(img), plt.title('Input')
             plt.subplot(122), plt.imshow(dst), plt.title('Output')
             plt.show()
         return dst
 
-
     def calibratePoint(self, coord):
-        coord_matrix = np.float32([[coord[0]],[coord[1]], [1]])
-        result_matrix = np.matmul(self.M,coord_matrix)
-        result = (result_matrix[0][0]/result_matrix[2][0],result_matrix[1][0]/result_matrix[2][0])
-        return result
-
-
-
+        coord_matrix = np.float32([[coord[0]], [coord[1]], [1]])
+        result_matrix = np.matmul(self.M, coord_matrix)
+        return result_matrix[0][0] / result_matrix[2][0], result_matrix[1][0] / result_matrix[2][0]
