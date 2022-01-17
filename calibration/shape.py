ab0e799086b0d4f9c0e5a4804c42d0211a198475
Benjamin-Eldo <84413803+Benjamin-Eldo@users.noreply.github.com>
2022-01-17 09:00:38 +0100

    Print coordinates of shape with minimum size

--- calibration/shape.py
+++ calibration/shape.py
@@ -13,39 +13,36 @@ while 1 :
     contours, _ = cv2.findContours(
         threshold, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
 
-    i = 0
 
     for contour in contours:
-        # cv2.approxPloyDP() function to approximate the shape
+
         approx = cv2.approxPolyDP(
             contour, 0.01 * cv2.arcLength(contour, True), True)
 
-        # using drawContours() function
+
         cv2.drawContours(img, [contour], 0, (0, 0, 255), 5)
         if cv2.contourArea(contour) > 1000:
 
-            if i == 0:
-                i = 1
-                continue
+            contour_corner = []
 
-
-
-            coin = approx.ravel()
+            corner = approx.ravel()
             i = 0
 
-            for j in coin:
+            for j in corner:
                 if (i % 2 == 0):
-                    x = coin[i]
-                    y = coin[i + 1]
+                    x = corner[i]
+                    y = corner[i + 1]
 
                     # String containing the co-ordinates.
                     string = str(x) + " " + str(y)
+                    contour_corner.append(string)
 
                     if (i != 0):
                         # text on remaining co-ordinates.
                         cv2.putText(img, string, (x, y),
                                     cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0))
                 i = i + 1
+            print(contour_corner)
 
         # putting shape name at center of each shape
         # if len(approx) == 3:
