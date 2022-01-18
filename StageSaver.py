5393c44c32926c69a8e82755958ae02037ba6bb0
gregory <greg@greg.com>
2022-01-18 17:30:02 +0100

    Stage saver class

---
+++ StageSaver.py
@@ -0,0 +1,17 @@
+from Stage import Stage
+
+class Stage_Saver:
+    def __init__(self, stage):
+        self.stage = stage
+
+    def save_header(self):
+        pass
+
+    def save_stage_header(self):
+        pass
+
+    def save_music_header(self):
+        pass
+
+    def save_targets(self):
+        pass
