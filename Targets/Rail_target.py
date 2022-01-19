2983a9332bd5228c2a11fd72d2266301f2605511
guillaume GRANDY <guillaume.grandy@gmail.com>
2022-01-19 13:01:56 +0100

    affichage des rails tragets

--- Targets/Rail_target.py
+++ Targets/Rail_target.py
@@ -1,5 +1,8 @@
+import pygame
+
 from Targets.Target import Target
 from Coordinates import  Coordinates
+from Constants import *
 
 class Rail_target(Target):
     def __init__(self, targetData,screen,picture):
@@ -14,3 +17,18 @@ class Rail_target(Target):
 
     def display(self):
         print(self.coordinates, self.steps, self.duration, self.delay, self.value, self.color)
+
+    def showTarget(self):
+        beginx=0
+        beginy=0
+        endx=0
+        endy=0
+        for point in self.steps:
+            beginx=endx
+            beginy=endy
+            endx=point.x
+            endy=point.y
+            if(beginx!=0 and beginy!=0):
+                pygame.draw.line(self.screen,(255,0,0),(beginx+Constants.TARGET_RADIUS,beginy+Constants.TARGET_RADIUS),(endx+Constants.TARGET_RADIUS,endy+Constants.TARGET_RADIUS),10)
+        for point in self.steps:
+            self.screen.blit(self.picture, (point.x, point.y))
